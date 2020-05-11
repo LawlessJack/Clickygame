c2a94678035efb7cf793b240cf006c63e4d7ec56
Jack Lawless <Lawless.jackm@gmail.com>
2020-05-11 13:39:56 -0500

    Updates

---
+++ precache-manifest.9eaf21d8c3474495d47b75a5ffb6df22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1732e6d55c1c08a95bf28e39563753b",
+    "url": "/Clickygame/index.html"
+  },
+  {
+    "revision": "508d722ec81b1fa1e3e0",
+    "url": "/Clickygame/static/css/main.e2996f3b.chunk.css"
+  },
+  {
+    "revision": "e4810c97df74eb9cc33d",
+    "url": "/Clickygame/static/js/2.326c3dd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clickygame/static/js/2.326c3dd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508d722ec81b1fa1e3e0",
+    "url": "/Clickygame/static/js/main.c4eeda4b.chunk.js"
+  },
+  {
+    "revision": "50c1ee0d9009db3e5b9e",
+    "url": "/Clickygame/static/js/runtime-main.094a8185.js"
+  }
+]);
